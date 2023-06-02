ed2905b10b3ad0d5d789d23c8d89e44ca916d176
nabeel <nabeel3333n@gmail.com>
2023-06-02 11:41:13 +0530

    mathjaxview updated

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
-import React from 'react';
-import { View } from 'react-native';
-import { WebView } from 'react-native-webview';
+import React from "react";
+import { View } from "react-native";
+import { WebView } from "react-native-webview";
 
 const defaultOptions = {
-	messageStyle: 'none',
-	extensions: ['tex2jax.js'],
-	jax: ['input/TeX', 'output/HTML-CSS'],
-	tex2jax: {
-		inlineMath: [['$', '$'], ['\\(', '\\)']],
-		displayMath: [['$$', '$$'], ['\\[', '\\]']],
-		processEscapes: true,
-	},
-	TeX: {
-		extensions: ['AMSmath.js', 'AMSsymbols.js', 'noErrors.js', 'noUndefined.js']
-	}
+  messageStyle: "none",
+  extensions: ["tex2jax.js"],
+  jax: ["input/TeX", "output/HTML-CSS"],
+  tex2jax: {
+    inlineMath: [
+      ["$", "$"],
+      ["\\(", "\\)"],
+    ],
+    displayMath: [
+      ["$$", "$$"],
+      ["\\[", "\\]"],
+    ],
+    processEscapes: true,
+  },
+  TeX: {
+    extensions: [
+      "AMSmath.js",
+      "AMSsymbols.js",
+      "noErrors.js",
+      "noUndefined.js",
+    ],
+  },
 };
 
 class MathJax extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			height: 1
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      height: 1,
+    };
+  }
 
-	handleMessage(message) {
-		this.setState({
-			height: Number(message.nativeEvent.data)
-		});
-	}
+  handleMessage(message) {
+    this.setState({
+      height: Number(message.nativeEvent.data),
+    });
+  }
 
-	wrapMathjax(content) {
-		const options = JSON.stringify(
-			Object.assign({}, defaultOptions, this.props.mathJaxOptions)
-		);
+  wrapMathjax(content) {
+    const options = JSON.stringify(
+      Object.assign({}, defaultOptions, this.props.mathJaxOptions)
+    );
+    // <script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js"></script>
 
-		return `
+    return `
 			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 			<script type="text/x-mathjax-config">
 				MathJax.Hub.Config(${options});
@@ -47,29 +59,35 @@ class MathJax extends React.Component {
 				});
 			</script>
 
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js"></script>
 			<div id="formula" style="visibility: hidden;">
 				${content}
 			</div>
 		`;
-	}
-	render() {
-		const html = this.wrapMathjax(this.props.html);
+  }
+  render() {
+    const html = this.wrapMathjax(this.props.html);
 
-		// Create new props without `props.html` field. Since it's deprecated.
-		const props = Object.assign({}, this.props, { html: undefined });
+    const INJECTED_JAVASCRIPT = `
+    let mathjaxScriptPath = 'file:///android_asset/MathJaxView/mathJaxView.min.js'
+    var script = document.createElement('script');
+    script.src =mathjaxScriptPath;
+     document.appendChild(script);`;
 
-		return (
-			<View style={{ height: this.state.height, ...props.style }}>
-				<WebView
-					scrollEnabled={false}
-					onMessage={this.handleMessage.bind(this)}
-					source={{ html }}
-					{...props}
-				/>
-			</View>
-		);
-	}
+    // Create new props without `props.html` field. Since it's deprecated.
+    const props = Object.assign({}, this.props, { html: undefined });
+
+    return (
+      <View style={{ height: this.state.height, ...props.style }}>
+        <WebView
+          scrollEnabled={false}
+          onMessage={this.handleMessage.bind(this)}
+          source={{ html }}
+          injectedJavaScript={INJECTED_JAVASCRIPT}
+          {...props}
+        />
+      </View>
+    );
+  }
 }
 
 export default MathJax;
